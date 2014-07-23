302bd873e2fdec6569ae2c475424a4722b8e646c
Aubrey Taylor <ataylor@blitzagency.com>
2014-07-23 11:43:55 -0700

    initial commit

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,15 +1,16 @@
-
-
 define(function(require, exports, module){
 
+var $ = require('jquery');
 var marionette = require('marionette');
 var renderer = require('app/renderer');
 var app = require('app/app');
 var AppRouter = require('app/app-router').AppRouter;
-require('backbone/stickit');
 
 app.appRouter = new AppRouter();
-app.start();
+
+$(function(){
+    app.start();
+});
 
 }); // eof define
 
